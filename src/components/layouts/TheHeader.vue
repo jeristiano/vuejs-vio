032a7038f23297cf66324fefcddda6bee04082d3
jeristiano <jeremykuang@foxmail.com>
2021-04-20 14:54:44 +0800

    注册页面

--- src/components/layouts/TheHeader.vue
+++ src/components/layouts/TheHeader.vue
@@ -9,26 +9,40 @@
           <span class="icon-bar"></span>
         </button>
 
-        <a href="/" class="navbar-brand">
-        <img :src="logo.src" :alt="logo.title">
+        <router-link to="/" class="navbar-brand">
           <span class="title">{{ logo.title }}</span>
-        </a>
+          <img :src="logo.src" :alt="logo.title" />
+        </router-link>
       </div>
 
-      <div id="top-navbar-collapse" :class="['collapse', 'navbar-collapse', { in: showCollapsedNav }]">
+      <div
+        id="top-navbar-collapse"
+        :class="['collapse', 'navbar-collapse', { in: showCollapsedNav }]"
+      >
         <ul class="nav navbar-nav">
-          <li v-for="(item, index) in navList" :class="{ active: index === activeNavIndex }">
+          <li
+            v-for="(item, index) in navList"
+            :class="{ active: index === activeNavIndex }"
+          >
             <a href="#" @click="changeNavIndex(index)">{{ item }}</a>
           </li>
         </ul>
+        <!-- 入口组件 -->
+        <div class="navbar-right">
+          <TheEntry />
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import TheEntry from "@/components/layouts/TheEntry";
 export default {
   name: "TheHeader",
+  components: {
+    TheEntry,
+  },
   data() {
     return {
       logo: {
@@ -37,7 +51,7 @@ export default {
       },
       navList: ["社区", "头条", "问答", "教程"],
       activeNavIndex: 0,
-      showCollapsedNav: false
+      showCollapsedNav: false,
     };
   },
   beforeCreate() {
@@ -46,12 +60,12 @@ export default {
   },
   methods: {
     changeNavIndex(index) {
-      this.activeNavIndex = index
+      this.activeNavIndex = index;
     },
-     toggleNav() {
-      this.showCollapsedNav = !this.showCollapsedNav
-    }
-  }
+    toggleNav() {
+      this.showCollapsedNav = !this.showCollapsedNav;
+    },
+  },
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,13 +1,10 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
+import router from './router'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',//一个页面上的元素elements
-  components: { App }, //引入默认的组件单入口
-  template: '<App/>'
-})
+  router,
+  render: h => h(App),
+}).$mount('#app')
