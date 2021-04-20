3846a8b0473c044abb5bb1974483d76fbf058cec
jeristiano <jeremykuang@foxmail.com>
2021-04-20 15:45:54 +0800

    登录注册

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
 import router from './router'
+import './directives'
+import './components'
+import store from './store'
+// 引入插件
+import VueSweetalert2 from './plugins/vue-sweetalert2'
+
+// 使用插件
+Vue.use(VueSweetalert2)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app')
