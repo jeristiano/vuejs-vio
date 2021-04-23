4600b570d8b160e09a0906a04d4e9642d483851b
jeristiano <jeremykuang@foxmail.com>
2021-04-23 13:01:17 +0800

    用户修改

--- src/router/routes.js
+++ src/router/routes.js
@@ -18,4 +18,32 @@ export default [
       name: 'Login',
       component: () => import('@/views/auth/Login')
     },
+    // EditUsers
+  {
+    path: '/users/1/edit',
+    name: 'EditUsers',
+    component: () => import('@/views/users/Edit.vue'),
+    children: [
+      {
+        path: '',
+        name: 'EditProfile',
+        component: () => import('@/views/users/Profile.vue'),
+        meta: { auth: true }
+      },
+      // EditAvatar
+      {
+        path: '/users/1/edit_avatar',
+        name: 'EditAvatar',
+        component: () => import('@/views/users/Avatar.vue'),
+        meta: { auth: true }
+      },
+      // EditPassword
+      {
+        path: '/users/1/edit_password',
+        name: 'EditPassword',
+        component: () => import('@/views/users/Password.vue'),
+        meta: { auth: true }
+      }
+    ]
+  },
   ]
