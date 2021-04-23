271dac80b2d2dd69e7029c09703c65ac99c694ee
jeristiano <jeremykuang@foxmail.com>
2021-04-23 14:29:21 +0800

    文章增删改查茶

--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,15 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import ls from '../utils/localStorage'
 import router from '../router'
-
+// 引入 actions.js 的所有导出
+import * as moreActions from './actions'
 Vue.use(Vuex)
 
 const state = {
   user: ls.getItem('user'),
   // 添加 auth 来保存当前用户的登录状态
-  auth: ls.getItem('auth')
+  auth: ls.getItem('auth'),
+  articles: ls.getItem('articles')
 }
 
 const mutations = {
@@ -20,6 +22,11 @@ const mutations = {
   UPDATE_AUTH(state, auth) {
     state.auth = auth
     ls.setItem('auth', auth)
+  },
+  // 更改所有文章的事件类型
+  UPDATE_ARTICLES(state, articles) {
+    state.articles = articles
+    ls.setItem('articles', articles)
   }
 }
 
@@ -34,16 +41,34 @@ const actions = {
     commit('UPDATE_AUTH', false)
     router.push({ name: 'Home', params: { logout: true } })
   },
-    // 更新个人信息
-    updateUser({ state, commit }, user) {
-      const stateUser = state.user
-  
-      if (stateUser && typeof stateUser === 'object') {
-        user = { ...stateUser, ...user }
-      }
-  
-      commit('UPDATE_USER', user)
+  // 更新个人信息
+  updateUser({ state, commit }, user) {
+    const stateUser = state.user
+
+    if (stateUser && typeof stateUser === 'object') {
+      user = { ...stateUser, ...user }
     }
+
+    commit('UPDATE_USER', user)
+  },
+
+   // 使用对象展开运算符混入 moreActions
+   ...moreActions
+}
+
+
+// 添加 getters
+const getters = {
+  getArticleById: (state) => (id) => {
+    let articles = state.articles
+
+    if (Array.isArray(articles)) {
+      articles = articles.filter(article => parseInt(id) === parseInt(article.articleId))
+      return articles.length ? articles[0] : null
+    } else {
+      return null
+    }
+  }
 }
 
 
@@ -51,6 +76,7 @@ const actions = {
 const store = new Vuex.Store({
   state,
   mutations,
+  getters,
   actions
 })
 
--- src/views/articles/Create.vue
+++ src/views/articles/Create.vue
@@ -3,15 +3,17 @@
     <div class="blog-pages">
       <div class="col-md-12 panel">
         <div class="panel-body">
-          <h2 class="text-center">创作文章</h2>
+        <h2 class="text-center">{{ articleId ? '编辑文章' : '创作文章' }}</h2>
           <hr />
           <div data-validator-form>
             <div class="form-group">
               <input
-                v-validator.required="{ title: '标题' }"
+                v-model.trim="title"
+                v-validator:blur.required="{ title: '标题' }"
                 type="text"
                 class="form-control"
                 placeholder="请填写标题"
+                @input="saveTitle"
               />
             </div>
             <div class="form-group">
@@ -19,7 +21,9 @@
             </div>
             <br />
             <div class="form-group">
-              <button class="btn btn-primary" type="submit">发 布</button>
+              <button class="btn btn-primary" type="submit" @click="post">
+                发 布
+              </button>
             </div>
           </div>
         </div>
@@ -27,36 +31,115 @@
     </div>
   </div>
 </template>
-
 <script>
-import SimpleMDE from "simplemde";
-// 引入 highlight.js 的默认值
+import SimpleMDE from 'simplemde'
 import hljs from 'highlight.js'
+import ls from '@/utils/localStorage'
 
-// 添加全局变量
 window.hljs = hljs
 
 export default {
-  name: "Create",
+  name: 'Create',
+  data() {
+    return {
+      title: '', // 文章标题
+      content: '', // 文章内容
+      articleId: undefined // 文章 ID
+    }
+  },
+  beforeRouteEnter(to, from, next) {
+    next(vm => {
+      vm.setArticleId(vm.$route.params.articleId)
+    })
+  },
+  beforeRouteLeave(to, from, next) {
+    this.clearData()
+    next()
+  },
+  watch: {
+    '$route'(to) {
+      this.clearData()
+      this.setArticleId(to.params.articleId)
+    }
+  },
   mounted() {
     const simplemde = new SimpleMDE({
-      element: document.querySelector("#editor"),
-      placeholder:
-        "请使用 Markdown 格式书写 ;-)，代码片段黏贴时请注意使用高亮语法。",
+      element: document.querySelector('#editor'),
+      placeholder: '请使用 Markdown 格式书写 ;-)，代码片段黏贴时请注意使用高亮语法。',
       spellChecker: false,
       autoDownloadFontAwesome: false,
       autosave: {
         enabled: true,
-        uniqueId: "vuejs-essential",
+        uniqueId: 'vuejs-essential'
       },
       renderingConfig: {
-        codeSyntaxHighlighting: true,
-      },
-    });
-  },
-};
-</script>
+        codeSyntaxHighlighting: true
+      }
+    })
 
+    simplemde.codemirror.on('change', () => {
+      this.content = simplemde.value()
+    })
+
+    this.simplemde = simplemde
+  },
+  methods: {
+    saveTitle() {
+      ls.setItem('smde_title', this.title)
+    },
+    fillContent(articleId) {
+      const simplemde = this.simplemde
+      const smde_title = ls.getItem('smde_title')
+
+      if (articleId !== undefined) {
+        const article = this.$store.getters.getArticleById(articleId)
+
+        if (article) {
+          const { title, content } = article
+
+          this.title = smde_title || title
+          this.content = simplemde.value() || content
+          simplemde.value(this.content)
+        }
+      } else {
+        this.title = smde_title
+        this.content = simplemde.value()
+      }
+    },
+    post() {
+      const title = this.title
+      const content = this.content
+
+      if (title !== '' && content.trim() !== '') {
+        const article = {
+          title,
+          content
+        }
+
+        this.$store.dispatch('post', { article, articleId: this.articleId })
+        this.clearData()
+      }
+    },
+    clearData() {
+      this.title = ''
+      ls.removeItem('smde_title')
+      this.simplemde.value('')
+      this.simplemde.clearAutosavedValue()
+    },
+    setArticleId(articleId) {
+      const localArticleId = ls.getItem('articleId')
+
+      if (articleId !== undefined && !(articleId === localArticleId)) {
+        this.clearData()
+      }
+
+      this.articleId = articleId
+      this.fillContent(articleId)
+      ls.setItem('articleId', articleId)
+    }
+  }
+}
+</script>
 <style scoped>
 .blog-container {
   max-width: 980px;
