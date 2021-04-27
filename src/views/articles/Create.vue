77e5d4b9568b59732b7caad17295ab9267d5d778
jeristiano <jeristianokuang@gmail.com>
2021-04-27 17:01:10 +0800

    编辑文章和创建文章页面出现生命周期问题

--- src/views/articles/Create.vue
+++ src/views/articles/Create.vue
@@ -7,14 +7,19 @@
           <hr />
           <div data-validator-form>
             <div class="form-group">
-              <input v-model.trim="title" v-validator:blur.required="{ title: '标题' }" type="text" class="form-control" placeholder="请填写标题" @input="saveTitle"> 
+              <input
+                v-validator.required="{ title: '标题' }"
+                type="text"
+                class="form-control"
+                placeholder="请填写标题"
+              />
             </div>
             <div class="form-group">
               <textarea id="editor"></textarea>
             </div>
             <br />
             <div class="form-group">
-              <button class="btn btn-primary" type="submit" @click="post">发 布</button>
+              <button class="btn btn-primary" type="submit">发 布</button>
             </div>
           </div>
         </div>
@@ -23,80 +28,115 @@
   </div>
 </template>
 <script>
-import SimpleMDE from 'simplemde'
-import hljs from 'highlight.js'
-import ls from '@/utils/localStorage'
+import SimpleMDE from "simplemde";
+import hljs from "highlight.js";
+import ls from "@/utils/localStorage";
 
-window.hljs = hljs
+window.hljs = hljs;
 
 export default {
-  name: 'Create',
+  name: "Create",
   data() {
     return {
-      title: '', // 文章标题
-      content: '' // 文章内容
-    }
+      title: "", // 文章标题
+      content: "", // 文章内容
+      articleId: undefined, // 文章 ID
+    };
+  },
+  beforeRouteEnter(to, from, next) {
+    next((vm) => {
+      vm.setArticleId(vm.$route.params.articleId);
+    });
+  },
+  beforeRouteLeave(to, from, next) {
+    this.clearData();
+    next();
+  },
+  watch: {
+    $route(to) {
+      this.clearData();
+      this.setArticleId(to.params.articleId);
+    },
   },
   mounted() {
     const simplemde = new SimpleMDE({
-      element: document.querySelector('#editor'),
-      placeholder: '请使用 Markdown 格式书写 ;-)，代码片段黏贴时请注意使用高亮语法。',
+      element: document.querySelector("#editor"),
+      placeholder:
+        "请使用 Markdown 格式书写 ;-)，代码片段黏贴时请注意使用高亮语法。",
       spellChecker: false,
       autoDownloadFontAwesome: false,
       autosave: {
         enabled: true,
-        uniqueId: 'vuejs-essential'
+        uniqueId: "vuejs-essential",
       },
       renderingConfig: {
-        codeSyntaxHighlighting: true
-      }
-    })
+        codeSyntaxHighlighting: true,
+      },
+    });
 
-    simplemde.codemirror.on('change', () => {
-      this.content = simplemde.value()
-    })
+    simplemde.codemirror.on("change", () => {
+      this.content = simplemde.value();
+    });
 
-    this.simplemde = simplemde
-    this.fillContent()
+    this.simplemde = simplemde;
   },
   methods: {
     saveTitle() {
-      ls.setItem('smde_title', this.title)
+      ls.setItem("smde_title", this.title);
     },
-    fillContent() {
-      const simplemde = this.simplemde
-      const title = ls.getItem('smde_title')
+    fillContent(articleId) {
+      const simplemde = this.simplemde;
+      const smde_title = ls.getItem("smde_title");
 
-      if (title !== null) {
-        this.title = title
+      if (articleId !== undefined) {
+        const article = this.$store.getters.getArticleById(articleId);
+
+        if (article) {
+          const { title, content } = article;
+
+          this.title = smde_title || title;
+          this.content = simplemde.value() || content;
+          simplemde.value(this.content);
+        }
+      } else {
+        this.title = smde_title;
+        this.content = simplemde.value();
       }
-
-      this.content = simplemde.value()
     },
     post() {
-      const title = this.title
-      const content = this.content
+      const title = this.title;
+      const content = this.content;
 
-      if (title !== '' && content.trim() !== '') {
+      if (title !== "" && content.trim() !== "") {
         const article = {
           title,
-          content
-        }
+          content,
+        };
 
-        this.$store.dispatch('post', { article })
-        this.clearData()
+        this.$store.dispatch("post", { article, articleId: this.articleId });
+        this.clearData();
       }
     },
     clearData() {
-      this.title = ''
-      ls.removeItem('smde_title')
-      this.simplemde.value('')
-      this.simplemde.clearAutosavedValue()
-    }
-  }
-}
-</script>
+      this.title = "";
+      ls.removeItem("smde_title");
+      this.simplemde.value("");
+      this.simplemde.clearAutosavedValue();
+    },
+    setArticleId(articleId) {
+      const localArticleId = ls.getItem("articleId");
 
+      if (articleId !== undefined && !(articleId === localArticleId)) {
+        this.clearData();
+      }
+
+      this.articleId = articleId;
+      this.fillContent(articleId);
+      ls.setItem("articleId", articleId);
+    },
+  },
+};
+</script>
 <style scoped>
 .blog-container {
   max-width: 980px;
